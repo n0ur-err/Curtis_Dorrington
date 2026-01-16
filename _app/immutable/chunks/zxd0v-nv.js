da4f06197f855fe2163c7b75b195e1bfcf0faae6
Nour Benzahra <cloutroza@gmail.com>
2026-01-16 22:43:36 +0000

    Deploy static build

---
+++ _app/immutable/chunks/zxd0v-nv.js
@@ -0,0 +1,1 @@
+import{e}from"./BOPq1Y_T.js";e();
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: false
+		}),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				if (path.includes('favicon')) return;
+				throw new Error(message);
+			}
+		}
 	}
 };
 
